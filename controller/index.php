7c36668b47fcea1ea47a7d8a429035da5a0d9142
Zuazuito <marthahdezmartinez@gmail.com>
2026-04-24 01:38:29 -0600

    add(controller,view)
     Se agregaron controladores y vistas

---
+++ controller/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+*Gente con el requiere recuerden que ejecutamos y traemos codigo de otro php , en este caso la vista de home , que practicamente es un html porque necesito saber porque me carga un horrorsote :,D 
+
+*Se me olvida a diferencia del include el requiere se detiene cuando truena y el include solo marca error y continua , es todo :D
+*/
+$page = 'index';
+
+$currentPage = $_SERVER['REQUEST_URI'];
+
+require 'view/index.view.php';
