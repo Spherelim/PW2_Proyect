194119653c83a443f46ed51ba33ece7423a16204
Zuazuito <marthahdezmartinez@gmail.com>
2026-04-25 11:56:31 -0600

    add(conexion)

--- core/config.php
+++ core/config.php
@@ -3,10 +3,10 @@
 return [
     'db' => [
         'conexion_Mundiales' => [
-            'host' => '127.0.0.1',
+            'host' => 'localhoat',
             'dbname' => 'nlgo',
             'username' => 'root',
-            'password' => 'root',
+            'password' => 'Noe_050703',
             'charset' => 'utf8mb4'
         ]
     ],
